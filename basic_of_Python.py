6e32a9eafd839d4860408ae58002af4e43fff77e
Shoichiro Yamamoto <sho.monkeyfarm.1225@gmail.com>
2023-05-20 18:40:38 +0900

    Pythonの基礎(01_basic_of_Python)

---
+++ basic_of_Python.py
@@ -0,0 +1,23 @@
+# 日本語（Japanese）に変換する
+# Pythonの基本(basic of Python)
+# 変数の宣言は、変数名 = 値
+# 型の宣言は不要
+num = 1
+print(num)
+name = "Python"
+print(name)
+bool = True
+print(bool)
+# 型の確認はtype関数
+print(type(num))
+print(type(name))
+print(type(bool))
+# 型変換は、int(), str(), bool()
+print(int("1"))
+print(str(1))
+
+print("Hello\nWorld") # 改行
+print("Hello\tWorld") # タブ
+print("Hello\"World") # ダブルクォーテーション
+print("Hello\'World") # シングルクォーテーション
+print("Hello\\World") # バックスラッシュ
